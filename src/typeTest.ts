f3d372a8e0d058cfa2b772ab2e788d593098dc49
almaraubel <almar@aubel.nl>
2021-09-18 20:39:49 +0200

    promise

--- src/typeTest.ts
+++ src/typeTest.ts
@@ -2,13 +2,14 @@ type functionType = <T extends Array<any>, U>(...args: any) => U;
 type functionType2 = (...args: any[]) => any;
 
 export type Actions<A> = {
-  [k in keyof A]: A[k] extends (...args: infer P) => infer R
-    ? (...args: P) => R
+  [k in keyof A]: A[k] extends (...args: infer P) => Promise<infer R>
+    ? (...args: P) => Promise<R>
     : never;
 };
 
 const bar = {
-  fiets: (a: string, b: number) => "fiets",
+  fiets: async (a: string, b: number) => Promise.resolve("fiets"),
+  auto: () => "aaa",
 };
 
 const createstore = <T extends Actions<T>>(foo: T) => {
